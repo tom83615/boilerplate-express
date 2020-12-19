89c00eff0faf3f16c76d82990fa67aaf75fe2cab
JAM <tom83615@gmail.com>
2020-12-19 15:59:19 +0000

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
-console.log("Hello World");
 
+app.get("/", function(req, res) {
+  console.log("Hello World");
+  res.send('Hello Express');
+});
 
 
 
