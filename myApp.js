0981865182b13809b6aadcdad3882038ee2d62d4
JAM <tom83615@gmail.com>
2020-12-19 16:13:58 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
   const resPath = __dirname + "/views/index.html";
+
   console.log(resPath);
   res.sendfile(resPath);
 });
